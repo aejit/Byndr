4e3313d7499aed6fcc7a15f10f1594971c2c20e5
kannugo@gmail.com <!Mig29mkisu30>
2020-11-22 15:17:34 +0530

    Tool tip done

--- byndr-ui/src/components/common/sidebar.js
+++ byndr-ui/src/components/common/sidebar.js
@@ -1,3 +1,4 @@
+import SidebarPopper from "./sidebarPopper";
 import React from "react";
 // import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { makeStyles } from "@material-ui/core/styles";
@@ -149,62 +150,99 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SideBar(props) {
   // const theme = useTheme();
-  const anchorRef = React.useRef(null);
+  const discoverRef = React.useRef(null);
+  const feedRef = React.useRef(null);
+  const libRef = React.useRef(null);
+  const spaceRef = React.useRef(null);
+  const pathRef = React.useRef(null);
+  const bcastRef = React.useRef(null);
+  const lgRef = React.useRef(null);
+  const myAcctRef = React.useRef(null);
+  const [currentRef, setCurrentRef] = React.useState({});
+  const [popperTitle, setPopperTitle] = React.useState("");
+  const [popperContent, setPopperContent] = React.useState("");
   const open = props.open != null ? props.open : false;
   const [popoverOpen, setPopoverOpen] = React.useState(false);
   const classes = useStyles();
-  const [arrowRef, setArrowRef] = React.useState(null);
+
   const [arrow, setArrow] = React.useState(true);
 
   const handleDrawerClose = () => {
     props.setOpenDrawer(false);
   };
 
-  const handleClick = (event) => {
-    setPopoverOpen((popoverOpen) => !popoverOpen);
+  const handleMouseBlur = (event, control) => {
+    switch (control) {
+      case "discover":
+        setPopperTitle("Discover");
+        setPopperContent(
+          "Search like a Superstar with the multidimensional search sets Powered by search strings."
+        );
+        setCurrentRef(discoverRef);
+        break;
+      case "feed":
+        setPopperTitle("Topics Feed");
+        setPopperContent(
+          "Buyild y our personal feed out content sourced from verified creators."
+        );
+        setCurrentRef(feedRef);
+        break;
+      case "lib":
+        setPopperTitle("Library");
+        setPopperContent(
+          "Organise your resources with an easy retrieval process so you can stay on top of things."
+        );
+        setCurrentRef(libRef);
+        break;
+      case "space":
+        setPopperTitle("Learning Spaces");
+        setPopperContent(
+          "Create intuitive larning experiences with easy elements to learn efficiently."
+        );
+        setCurrentRef(spaceRef);
+        break;
+      case "path":
+        setPopperTitle("Discover");
+        setPopperContent(
+          "Search like a Superstar with the multidimensional search sets Powered by search strings."
+        );
+        setCurrentRef(pathRef);
+        break;
+      case "bcast":
+        setPopperTitle("Broadcast");
+        setPopperContent(
+          "React out ro your audience and Stay connected by going live or uploading content."
+        );
+        setCurrentRef(bcastRef);
+        break;
+      case "lg":
+        setPopperTitle("Learning Groups");
+        setPopperContent("Find like minded learners and join the community.");
+        setCurrentRef(lgRef);
+        break;
+      case "myacct":
+        setPopperTitle("My Account");
+        setPopperContent("Let's get you ready to learn.");
+        setCurrentRef(myAcctRef);
+        break;
+
+      default:
+        break;
+    }
+    setPopoverOpen(true);
   };
 
   return (
     <div className={classes.root}>
-      <Popper
-        id={"scroll-playground"}
-        open={popoverOpen}
-        anchorEl={anchorRef.current}
-        placement={"right"}
-        disablePortal={true}
-        className={classes.popper}
-        modifiers={{
-          flip: {
-            enabled: true,
-          },
-          preventOverflow: {
-            enabled: false,
-            boundariesElement: "scrollParent",
-          },
-          arrow: {
-            enabled: true,
-            element: arrowRef,
-          },
-        }}
-      >
-        {arrow ? <span className={classes.arrow} ref={setArrowRef} /> : null}
-        <Paper className={classes.paper}>
-          <DialogTitle>{"Use Google's location service?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Let Google help apps determine location.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClick} color="primary">
-              Disagree
-            </Button>
-            <Button onClick={handleClick} color="primary">
-              Agree
-            </Button>
-          </DialogActions>
-        </Paper>
-      </Popper>
+      <SidebarPopper
+        popoverOpen={popoverOpen}
+        setPopoverOpen={setPopoverOpen}
+        arrow={arrow}
+        handleClick={handleMouseBlur}
+        anchorRef={currentRef}
+        title={popperTitle}
+        content={popperContent}
+      />
       <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
@@ -246,49 +284,65 @@ export default function SideBar(props) {
           <Grid item>
             <List>
               <ListItem button key={"Discover"}>
-                <ListItemIcon>
-                  <FontAwesomeIcon
-                    onClick={handleClick}
-                    ref={anchorRef}
-                    icon={faGalacticRepublic}
-                    size={"lg"}
-                  />
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "discover")}
+                  ref={discoverRef}
+                >
+                  <FontAwesomeIcon icon={faGalacticRepublic} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary={"Discover"} />
               </ListItem>
 
               <ListItem button key={"Feed"}>
-                <ListItemIcon>
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "feed")}
+                  ref={feedRef}
+                >
                   <FontAwesomeIcon icon={faLayerGroup} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary={"Feed"} />
               </ListItem>
               <ListItem button key={"Library"}>
-                <ListItemIcon>
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "lib")}
+                  ref={libRef}
+                >
                   <FontAwesomeIcon icon={faBookmark} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary={"Library"} />
               </ListItem>
               <ListItem button key={"Spaces"}>
-                <ListItemIcon>
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "space")}
+                  ref={spaceRef}
+                >
                   <FontAwesomeIcon icon={faNewspaper} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary={"Spaces"} />
               </ListItem>
               <ListItem button key={"Paths"}>
-                <ListItemIcon>
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "path")}
+                  ref={pathRef}
+                >
                   <FontAwesomeIcon icon={faPaperPlane} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary={"Paths"} />
               </ListItem>
               <ListItem button key={"Broadcasts"}>
-                <ListItemIcon>
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "bcast")}
+                  ref={bcastRef}
+                >
                   <FontAwesomeIcon icon={faVideo} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary={"Broadcasts"} />
               </ListItem>
               <ListItem button key={"Learning Groups"}>
-                <ListItemIcon>
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "lg")}
+                  ref={lgRef}
+                >
                   <FontAwesomeIcon icon={faUsers} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary={"Learning Groups"} />
@@ -303,7 +357,10 @@ export default function SideBar(props) {
                 component={Link}
                 to="/profile/settings"
               >
-                <ListItemIcon>
+                <ListItemIcon
+                  onMouseEnter={(e) => handleMouseBlur(e, "myacct")}
+                  ref={myAcctRef}
+                >
                   <FontAwesomeIcon icon={faUserCircle} size={"lg"} />
                 </ListItemIcon>
                 <ListItemText primary="My Account"></ListItemText>
