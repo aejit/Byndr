ec79a121654235d9cd95ba2d26d240e52deda94e
Aejit <aejit486001@gmail.com>
2020-12-14 18:05:04 +0530

    HOme-screen updated

--- byndr-ui/src/components/common/navigationbar.js
+++ byndr-ui/src/components/common/navigationbar.js
@@ -2,15 +2,7 @@ import React from 'react';
 import { fade, makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import { Button } from '@material-ui/core';
 import Select from "react-select";
 
 const useStyles = makeStyles((theme) => ({
@@ -143,59 +135,7 @@ const customStyles = {
 export default function Navigationbar() {
 
     const classes = useStyles();
-    // const [anchorEl, setAnchorEl] = React.useState(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-    // const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-    // const handleProfileMenuOpen = (event) => {
-    //     setAnchorEl(event.currentTarget);
-    // };
-
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
-
-    // const handleMenuClose = () => {
-    //     setAnchorEl(null);
-    //     handleMobileMenuClose();
-    // };
-
-    const handleMobileMenuOpen = (event) => {
-        setMobileMoreAnchorEl(event.currentTarget);
-    };
-
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-        <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={mobileMenuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}
-        >
-            <MenuItem>
-                <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="secondary">
-                        <MailIcon />
-                    </Badge>
-                </IconButton>
-                <p>Messages</p>
-            </MenuItem>
-            <MenuItem>
-                <IconButton aria-label="show 11 new notifications" color="inherit">
-                    <Badge badgeContent={11} color="secondary">
-                        <NotificationsIcon />
-                    </Badge>
-                </IconButton>
-                <p>Notifications</p>
-            </MenuItem>
-            
-        </Menu>
-    );
 
     return (
         <div className={classes.grow}>
@@ -263,43 +203,8 @@ export default function Navigationbar() {
                         </div>
                     </div>
 
-                    <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
-                        <Button style={{ textTransform: 'none', color: 'lightgrey' }} >+ Invite People</Button>
-                        <IconButton aria-label="show 4 new mails" color="inherit">
-                            <Badge badgeContent={4} color="secondary">
-                                <MailIcon style={{ color: 'lightgrey' }}
-                                    onMouseOver={(e) => e.target.style.color = 'blue'}
-                                    onMouseOut={(e) => e.target.style.color = 'lightgrey'}
-                                />
-                            </Badge>
-                        </IconButton>
-
-                        <IconButton aria-label="show 17 new notifications" color="inherit" >
-                            <Badge badgeContent={17} color="secondary">
-                                <NotificationsIcon
-                                    style={{ color: 'lightgrey' }}
-                                    onMouseOver={(e) => e.target.style.color = 'blue'}
-                                    onMouseOut={(e) => e.target.style.color = 'lightgrey'}
-                                />
-                            </Badge>
-                        </IconButton>
-                       
-                    </div>
-                    <div className={classes.sectionMobile}>
-                        <IconButton
-                            aria-label="show more"
-                            aria-controls={mobileMenuId}
-                            aria-haspopup="true"
-                            onClick={handleMobileMenuOpen}
-                            color="inherit"
-                        >
-                            <MoreIcon />
-                        </IconButton>
-                    </div>
                 </Toolbar>
             </AppBar>
-            {renderMobileMenu}
         </div>
     );
 }
